3a23f3f48b24b0809644a268e6dd3bcef09d8081
Alexandre <alexandre.bouvier@epitech.eu>
2022-02-11 22:53:31 +0100

    Update S3-manager.module.ts

--- src/S3/S3-manager.module.ts
+++ src/S3/S3-manager.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { AwsSdkModule } from 'nest-aws-sdk';
 import { S3 } from 'aws-sdk';
-import { S3ManagerService } from './s3-manager.service';
+import {S3ManagerService} from "./S3-manager.service";
 
 @Module({
     imports: [AwsSdkModule.forFeatures([S3])],
