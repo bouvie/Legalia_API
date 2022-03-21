fa9b8966103e91248190fae5f10435a34b888eb7
Alexandre <alexandre.bouvier@epitech.eu>
2022-03-21 15:04:14 +0100

    Update fileEntity.service.ts

--- src/fileEntity/fileEntity.service.ts
+++ src/fileEntity/fileEntity.service.ts
@@ -7,13 +7,20 @@ import {LgDocument} from "../lgTemplates/lgDocument/lgDocument.model";
 
 @Injectable()
 export class FileEntityService {
+    private s3 = new S3({
+        "accessKeyId" : process.env.AWS_ACCESS_KEY_ID,
+        "secretAccessKey" : process.env.AWS_SECRET_ACCESS_KEY,
+        "signatureVersion": 'v4',
+        "region": process.env.AWS_REGION
+    });
+
+
     constructor(@InjectModel(FileEntity.name) private readonly fileEntityModel: Model<FileEntity>) {}
 
     async uploadPublicFile(dataBuffer, filename, computed ? : boolean) {
         const file : FileEntity = await this.fileEntityModel.create({filename : filename});
-        const s3 = new S3();
         const key = computed ? file._id.toString() + "_computed.docx" : file._id.toString();
-        const uploadResult = await s3.upload({
+        const uploadResult = await this.s3.upload({
             Bucket: process.env.S3_BUCKET_NAME,
             Body: dataBuffer.buffer,
             Key: key,
@@ -26,13 +33,12 @@ export class FileEntityService {
     }
 
     async downloadFile(file : FileEntity) {
-        const s3 = new S3();
         const downloadParams = {
                 Key: file.key,
                 Bucket: process.env.S3_BUCKET_NAME,
             };
         return new Promise<string>((resolve, reject) => {
-            s3.getSignedUrl('getObject', downloadParams, function (err, url) {
+            this.s3.getSignedUrl('getObject', downloadParams, function (err, url) {
                 resolve(url);
             });
         });
